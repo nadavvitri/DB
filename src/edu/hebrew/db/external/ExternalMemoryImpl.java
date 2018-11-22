7f4f4a0b0b529570b40d6c8cc04af08aa7ebde39
Nadav Vitri <nadav.vitri@cs.huji.ac.il>
2018-11-22 17:51:05 +0200

    complited first stage

--- src/edu/hebrew/db/external/ExternalMemoryImpl.java
+++ src/edu/hebrew/db/external/ExternalMemoryImpl.java
@@ -1,50 +1,74 @@
 package edu.hebrew.db.external;
 
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class ExternalMemoryImpl implements IExternalMemory {
 
-	private static final int M = 1;
-	private static final int Y = 200;
-	private static final int RAM = 50000000;  // 50Mb in bytes
-	private static final int COLSIZE = 20;
+	private static final int blocksNum = 1000;
+	private static final int bytesInBlock = 20000;
 
 	private String getField(String line, int colNum){
-		int from = (colNum - 1) * (COLSIZE + 1) + 1;
-		if (colNum == 1) {
-			from = 0;
-		}
-		int end = from + COLSIZE;
-		return line.substring(from, end);
+		return line.split(" ")[colNum - 1];
 	}
 
-	private void firstStage(BufferedReader reader, String in, String out, int colNum, String tmpPath, int readChunks){
-		long iterations = Math.ceil(reader.length() / RAM)
+	private void firstStage(BufferedReader reader, BufferedWriter writer, int colNum, int readChunks, long linesNum) throws Exception{
+
+		double iterations = Math.ceil((double)linesNum / readChunks);
 		for (int i = 0; i < iterations; i++) {
 			Map<String, String> map = new TreeMap<String, String>();
+
+			// Read chunk of lines from file to hash table
 			for (int row = 0; row < readChunks; row++) {
 				String line = "";
-				line=reader.readLine()
-				map.put(getField(line, colNum),line);
-
+				if ((line = reader.readLine()) != null) {
+					if (map.containsKey(getField(line, colNum))) {
+						String key = getField(line, colNum);
+						map.put(key, map.get(key) + System.lineSeparator() + line);
+					}
+					else{
+						map.put(getField(line, colNum), line);
+					}
+				}
+				else {
+					break;
+				}
 			}
 
+			// Write lines sorted by key to tmp
+			for (String l : map.values()){
+				writer.write(l);
+				writer.newLine();
+			}
 		}
-
-
 	}
 
 	@Override
 	public void sort(String in, String out, int colNum, String tmpPath) {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(in));
-			int lineLength = reader.readLine().length();
-			int readChunks = Math.ceil(RAM / (lineLength * 2)); // lines can go into RAM
-			reader.reset();
-			firstStage(reader, in, out, colNum, tmpPath, readChunks);
+			File file = new File(in);
+			BufferedReader reader = new BufferedReader(new FileReader(file));
 
+			File fout = new File(tmpPath + "tmp.txt");
+			FileOutputStream fos = new FileOutputStream(fout);
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+
+			int lineLength = reader.readLine().length();
+			int readChunks = (int) Math.ceil((bytesInBlock * blocksNum) / (lineLength * 2)); // lines can go into RAM
+
+
+			// TODO: back to first line
+			//reader.mark(0);
+			//reader.reset();
+			reader = new BufferedReader(new FileReader(file));
+			firstStage(reader, writer, colNum, readChunks, (file.length() / lineLength));
+			writer.close();
 		}
+
 		catch(Exception e) {
 			e.printStackTrace();
 		}
