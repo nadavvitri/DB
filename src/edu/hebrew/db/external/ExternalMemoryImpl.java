393e4162f4040372bc923e0254aa8aa570facec3
Nadav Vitri <nadav.vitri@cs.huji.ac.il>
2018-11-22 13:51:46 +0200

    frist stage

--- src/edu/hebrew/db/external/ExternalMemoryImpl.java
+++ src/edu/hebrew/db/external/ExternalMemoryImpl.java
@@ -7,8 +7,31 @@ public class ExternalMemoryImpl implements IExternalMemory {
 
 	private static final int M = 1;
 	private static final int Y = 200;
+	private static final int RAM = 50000000;  // 50Mb in bytes
+	private static final int COLSIZE = 20;
 
-	private void firstStage(String in, String out, int colNum, String tmpPath, int readChunks){
+	private String getField(String line, int colNum){
+		int from = (colNum - 1) * (COLSIZE + 1) + 1;
+		if (colNum == 1) {
+			from = 0;
+		}
+		int end = from + COLSIZE;
+		return line.substring(from, end);
+	}
+
+	private void firstStage(BufferedReader reader, String in, String out, int colNum, String tmpPath, int readChunks){
+		long iterations = Math.ceil(reader.length() / RAM)
+		for (int i = 0; i < iterations; i++) {
+			Map<String, String> map = new TreeMap<String, String>();
+			for (int row = 0; row < readChunks; row++) {
+				String line = "";
+				line=reader.readLine()
+				map.put(getField(line, colNum),line);
+
+			}
+
+		}
+
 
 	}
 
@@ -16,9 +39,10 @@ public class ExternalMemoryImpl implements IExternalMemory {
 	public void sort(String in, String out, int colNum, String tmpPath) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(in));
-			//int lineLength = reader.readLine().length();
-			//int readChunks = (lineLength / Y) * M;
-			//reader.reset();
+			int lineLength = reader.readLine().length();
+			int readChunks = Math.ceil(RAM / (lineLength * 2)); // lines can go into RAM
+			reader.reset();
+			firstStage(reader, in, out, colNum, tmpPath, readChunks);
 
 		}
 		catch(Exception e) {
